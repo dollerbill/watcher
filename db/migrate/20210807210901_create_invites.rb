7a1f89d43625058b4c6be72e13b8284767913f15
Billy Cropley <billycropley@gmail.com>
2021-08-16 01:58:50 -0700

    Fix Invite signup process

--- db/migrate/20210807210901_create_invites.rb
+++ db/migrate/20210807210901_create_invites.rb
@@ -7,7 +7,7 @@ class CreateInvites < ActiveRecord::Migration[6.1]
     create_table :invites do |t|
       t.string :invite_name, null: false
       t.citext :invite_email, null: false, index: { unique: true }
-      t.string :token, null: false, index: { unique: true }
+      t.string :token, index: { unique: true }
       t.references :invited_by_user, null: false
       t.timestamps
       t.timestamp :accepted_at, index: true
