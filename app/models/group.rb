b2b131f55ce3ca6a981d428ccad0aa7efe50d329
Billy Cropley <billycropley@gmail.com>
2022-03-31 22:40:53 -0700

    Remove rated

--- app/models/group.rb
+++ app/models/group.rb
@@ -12,8 +12,4 @@ class Group < ApplicationRecord
   has_many :users
   has_many :user_reactions, through: :users
   has_many :movies, through: :user_reactions
-
-  def match?
-    user_reactions.select(&:movie).select(&:positive?)
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'rated' => 'movies#rated'
   get 'recommended' => 'movies#recommended'
   get 'welcome' => 'users#welcome'
   post 'reaction' => 'user_reactions#rate'
